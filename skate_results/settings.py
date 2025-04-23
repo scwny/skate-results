7c13e6659d1857a435fdaf71709701f9e03d1bc0
scwny <president@scwny.org>
2025-04-23 13:33:49 -0400

    Updated AWS config

--- skate_results/settings.py
+++ skate_results/settings.py
@@ -94,6 +94,18 @@ DATABASES = {
 }
 
 
+
+# Media via S3
+INSTALLED_APPS += ['storages']
+
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME    = os.getenv('AWS_S3_REGION_NAME')
+AWS_S3_CUSTOM_DOMAIN  = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_DEFAULT_ACL       = None
+
+MEDIA_URL   = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
