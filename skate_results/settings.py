fe2262287e02614ebfe251e9b4ee83ea08474fd8
scwny <president@scwny.org>
2025-04-24 12:37:34 -0400

    Fixed a syntax error in allowed hosts

--- skate_results/settings.py
+++ skate_results/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['skate-results-app.azurewebsites.net', '127.0.0.1', 'mayskateresults.scwny.org',"*.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ['skate-results-app.azurewebsites.net', '127.0.0.1', 'mayskateresults.scwny.org',".elasticbeanstalk.com"]
 
 
 # Application definition
