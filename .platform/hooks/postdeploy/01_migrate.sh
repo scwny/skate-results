87b5c1bc29f5e4141bc10aaae080f1dbdafb59ef
scwny <president@scwny.org>
2025-04-24 11:47:19 -0400

    Add EB post-deploy hook to run Django migrations

---
+++ .platform/hooks/postdeploy/01_migrate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Runs every time EB successfully deploys a new application version.
+source /var/app/venv/*/bin/activate        # activates the virtual-env EB created
+cd /var/app/current                         # the app code directory
+python manage.py migrate --noinput
