34c6f73c844d1e371d865acd26e8c2a9ebc65211
scwny <president@scwny.org>
2025-04-24 13:27:47 -0400

    Trying to fix static files

--- skate_results/settings.py
+++ skate_results/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Load .env file explicitly if needed
 #config = Config(repository=RepositoryEnv('.env'))
@@ -139,7 +140,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL  = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
