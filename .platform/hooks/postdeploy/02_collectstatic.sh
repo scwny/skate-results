8cb5dd4201a3c435e9234d250850bb4d54f379c6
scwny <president@scwny.org>
2025-04-25 07:23:31 -0400

    Fix collectstatic hook: load env & run as webapp

--- .platform/hooks/postdeploy/02_collectstatic.sh
+++ .platform/hooks/postdeploy/02_collectstatic.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
-sudo -u webapp -i bash <<'INNER'
-source /var/app/venv/*/bin/activate
-cd /var/app/current
-python manage.py collectstatic --noinput
-INNER
+set -e
+
+# 1) Load the Elastic Beanstalk environment variables
+#    (creates DB_HOST, SECRET_KEY, etc. for this shell)
+if [ -f /opt/elasticbeanstalk/deployment/env ]; then
+  source /opt/elasticbeanstalk/deployment/env
+fi
+
+# 2) Run collectstatic as the 'webapp' user so permissions are correct
+sudo -u webapp -E bash -c '
+  source /var/app/venv/*/bin/activate
+  cd /var/app/current
+  python manage.py collectstatic --noinput
+'
