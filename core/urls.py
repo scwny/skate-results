35611bb16b530950386ab30f5ecdec2c1d192bd7
scwny <president@scwny.org>
2025-04-25 13:52:25 -0400

    Added competition list, needs cleanup

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-from .views import EventListView, EventScheduleView, ping, EventResultsView
+from .views import CompetitionListView, CompetitionEventListView, EventScheduleView, ping, EventResultsView
 
 
 urlpatterns = [
     path('ping', ping, name='Ping'),
-    path('',                  EventListView.as_view(),      name='event_list'),
-    path('event/<int:pk>/schedule/',      EventScheduleView.as_view(), name='event_schedule'),
-    path('event/<int:pk>/results/',       EventResultsView.as_view(),  name='event_results'),
+        
+    # 1) New root → list of Competitions
+    path('',CompetitionListView.as_view(),name='competition_list'),
+
+    # 2) Competition detail → its Events
+    path('competition/<int:pk>/',CompetitionEventListView.as_view(),name='competition_events'),
+
+    # 3) Individual Event schedule 
+    path('event/<int:pk>/schedule/', EventScheduleView.as_view(), name='event_schedule'),
+    
+    # 4) Event Results
+    path('event/<int:pk>/results/', EventResultsView.as_view(),  name='event_results'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from decouple import config
 from django.views.generic import ListView, DetailView
-from .models import Event
+from .models import Competition, Event
 
 # Create your views here.
 from django.http import HttpResponse
@@ -42,4 +42,25 @@ class EventResultsView(DetailView):
         ctx['scheduled_skaters'] = self.object.event.all()
         # only show image if finished and has one
         ctx['has_results'] = bool(self.object.status == 'finished' and self.object.result_image)
+        return ctx
+
+class CompetitionListView(ListView):
+    model               = Competition
+    context_object_name = 'competitions'
+    template_name       = 'core/competition_list.html'
+    paginate_by         = 20  # optional
+
+class CompetitionEventListView(ListView):
+    model               = Event
+    context_object_name = 'events'
+    template_name       = 'core/event_list.html'
+    paginate_by         = 25
+
+    def get_queryset(self):
+        return Event.objects.filter(competition_id=self.kwargs['pk'])
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        # so your event_list.html can show the parent competition
+        ctx['competition'] = Competition.objects.get(pk=self.kwargs['pk'])
         return ctx
